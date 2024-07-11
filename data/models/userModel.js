321404883d83220e089aaa87689a433bd5351a4f
cecilialsantin <chikhakituti@gmail.com>
2024-07-11 14:17:50 -0300

    Eliminar archivos ignorados y añadir .gitignore

--- data/models/userModel.js
+++ data/models/userModel.js
@@ -12,6 +12,14 @@ const User = sequelize.define('User', {
     allowNull: false,
     unique: true
   },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
   password: {
     type: DataTypes.STRING,
     allowNull: false
---
+++ readme.md
@@ -0,0 +1,5 @@
+Para iniciar
+
+npm init -y
+
+npm install express bcryptjs jsonwebtoken sequelize mysql2 dotenv
